f68f4cdf3a059f58c865954c93ba08cbe6c13a62
attapun-an <attapunanivat@gmail.com>
2018-01-29 23:47:18 +0700

    I'm so confused. it works but. Still don't get it.
    meow

--- task_25_02.py
+++ task_25_02.py
@@ -1,13 +1,22 @@
 import time
 
-# supposed to convert to binary
+# denary to binary converter
 def X(n):
     if n == 0 or n == -1:
         print(n)
     else:
+        print("{0} is not 0 or 1".format(n))
+        time.sleep(1)
+        print("{0} MOD 2 = {1}".format(n, n//2))
+        time.sleep(1)
+
         X(n // 2)
-        print(n % 2)
+        print("n % 2 = {0}".format(n % 2))
+
+
 
 X(12)
+
+
 
 
