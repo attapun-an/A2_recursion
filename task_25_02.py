5525952a8b577d78a56b45c523313b8faad02b6d
attapun-an <attapunanivat@gmail.com>
2018-01-30 08:50:07 +0700

    minus the minus
    YOU ACCIDENTALLY INCLUDED A “-“ in front of the 1 …

--- task_25_02.py
+++ task_25_02.py
@@ -2,12 +2,12 @@ import time
 
 # denary to binary converter
 def X(n):
-    if n == 0 or n == -1:
+    if n == 0 or n == 1:
         print("it equals 0 or one")
         time.sleep(1)
-        print("         {0}".format(n))
+        print("        {0}".format(n))
     else:
-        print("{0} was not 0".format(n))
+        print("{0} was not 0 or 1".format(n))
         time.sleep(1)
 
         print("{0} // 2 = {1}".format(n, n // 2))
